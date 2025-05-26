aa02ac2d92643067e0b6514d6e128640a45b7b58
Irivng Uriel Espinosa Hernandez <20223tn050@utez.edu.mx>
2025-05-26 17:48:32 -0600

    [First Commit] Subida de proyecto

---
+++ src/main/java/utez/edu/mx/cviueh9a/Cviueh9aApplication.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.cviueh9a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cviueh9aApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cviueh9aApplication.class, args);
+	}
+
+}
---
+++ src/test/java/utez/edu/mx/cviueh9a/Cviueh9aApplicationTests.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.cviueh9a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cviueh9aApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
